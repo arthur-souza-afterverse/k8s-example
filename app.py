96f67250ccceb301778891b15a9d757ac66fb487
arthur-souza-afterverse <89874999+arthur-souza-afterverse@users.noreply.github.com>
2022-01-06 11:30:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Flask inside Docker - K8s dAfterverse !!"
+    return "Flask inside K8S - Afterverse !!"
 
 
 if __name__ == "__main__":
